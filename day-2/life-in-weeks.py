732256b1070ad864ac3fc36091d936ae2344e424
Lawrence Dovin <lawrencedovin@gmail.com>
2021-01-07 22:55:42 -0500

    Updated day 2 challenge life in weeks challenge

--- day-2/life-in-weeks.py
+++ day-2/life-in-weeks.py
@@ -1,3 +1,9 @@
 age = input("What is your current age? > ")
 age_in_weeks = int(age)*52
+
+years_remaining = 90 - int(age)
+days_remaining = years_remaining * 365
+weeks_remaining = years_remaining * 52
+months_remaining = years_remaining * 12
 print(f'You are {age_in_weeks} weeks old.')
+print(f'You have {days_remaining} days, {weeks_remaining} weeks and {months_remaining} months remaining.')
