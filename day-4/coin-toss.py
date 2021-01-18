8f95da8f83473ca8aa10c5d8654f12e9aaf55f0e
Lawrence Dovin <lawrencedovin@gmail.com>
2021-01-17 22:12:18 -0500

    Added day 4 heads or tails

---
+++ day-4/coin-toss.py
@@ -0,0 +1,5 @@
+from random import randint
+
+head_or_tails = randint(1,2)
+
+print('Heads') if head_or_tails == 1 else print('Tails')
---
+++ day-4/randomz.py
@@ -0,0 +1,7 @@
+from random import randint
+from module import PI
+
+random_integer = randint(1, 10)
+print(random_integer)
+
+print(PI)
