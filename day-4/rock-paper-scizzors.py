cb2c7b0c4efec4ecc261083ab77117cbf3e0f842
Lawrence Dovin <lawrencedovin@gmail.com>
2021-01-22 23:33:38 -0500

    Refactored rock paper scizzors

--- day-4/rock-paper-scizzors.py
+++ day-4/rock-paper-scizzors.py
@@ -35,17 +35,11 @@ computer_choice = random.randint(0, 2)
 print("Computer chose:")
 print(game_images[computer_choice])
 
-if user_choice >= 3 or user_choice < 0: 
-  print("You typed an invalid number, you lose!") 
-elif user_choice == 0 and computer_choice == 2:
+if user_choice == 0 and computer_choice == 2 or user_choice > computer_choice:
   print("You win!")
-elif computer_choice == 0 and user_choice == 2:
+elif user_choice >= 3 or user_choice < 0 or computer_choice == 0 and user_choice == 2 or computer_choice > user_choice:
   print("You lose")
-elif computer_choice > user_choice:
-  print("You lose")
-elif user_choice > computer_choice:
-  print("You win!")
-elif computer_choice == user_choice:
+else:
   print("It's a draw")
 
 ####### Debugging challenge: #########
