218d1329445e2537ae3c71a8d658f1f7b3c852be
Lawrence Dovin <lawrencedovin@gmail.com>
2021-01-24 21:01:48 -0500

    Added high score challenge

---
+++ day-5/high-score.py
@@ -0,0 +1,9 @@
+student_scores = input('Input a list of student scores: ').split()
+student_scores = [int(score) for score in student_scores]
+
+high_score = 0
+for score in student_scores:
+    if score > high_score:
+        high_score = score
+
+print(high_score)
