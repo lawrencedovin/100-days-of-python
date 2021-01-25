cec57b10c7568279d35aee1e582121a811ae0029
Lawrence Dovin <lawrencedovin@gmail.com>
2021-01-24 21:03:22 -0500

    updated high score

--- day-5/high-score.py
+++ day-5/high-score.py
@@ -1,4 +1,4 @@
-student_scores = input('Input a list of student scores: ').split()
+student_scores = input('Input a list of student scores: ').split(', ')
 student_scores = [int(score) for score in student_scores]
 
 high_score = 0
