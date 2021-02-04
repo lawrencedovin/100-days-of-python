709391b6f257b1bc4994a5f63b60790277ab50f9
Lawrence Dovin <lawrencedovin@gmail.com>
2021-02-03 21:18:29 -0500

    Updated prime calc solution

--- day-8/prime.py
+++ day-8/prime.py
@@ -6,8 +6,7 @@ not_prime_msg = " is not a prime number"
 def prime_number(number):
     whole_number_counter = 0
     for current_number in range(1, 100):
-        if (number / current_number).is_integer():
-            whole_number_counter += 1
+        whole_number_counter += 1 if (number / current_number).is_integer() else 0
     
     print(f"{number}{not_prime_msg}") if whole_number_counter > 2 else print(f"{number}{prime_msg}")
 
