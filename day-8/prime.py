7014425a7e6b28316b913a1c9a4f11d9137a67b4
Lawrence Dovin <lawrencedovin@gmail.com>
2021-02-03 21:16:14 -0500

    Added prime calculator

---
+++ day-8/prime.py
@@ -0,0 +1,14 @@
+number = int(input("Check if a number is prime: "))
+
+prime_msg = " is a prime number"
+not_prime_msg = " is not a prime number"
+
+def prime_number(number):
+    whole_number_counter = 0
+    for current_number in range(1, 100):
+        if (number / current_number).is_integer():
+            whole_number_counter += 1
+    
+    print(f"{number}{not_prime_msg}") if whole_number_counter > 2 else print(f"{number}{prime_msg}")
+
+prime_number(number)
