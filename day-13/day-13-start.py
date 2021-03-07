9e654c69d6915f9aa71dd2274bed0402f55e1784
Lawrence Dovin <lawrencedovin@gmail.com>
2021-03-06 20:56:47 -0500

    Fixed all errors

--- day-13/day-13-start.py
+++ day-13/day-13-start.py
@@ -2,7 +2,7 @@
 
 # # Describe Problem
 # def my_function():
-#   for i in range(1, 20):
+#   for i in range(1, 21):
 #     if i == 20:
 #       print("You got it")
 # my_function()
@@ -10,7 +10,7 @@
 # # Reproduce the Bug
 # from random import randint
 # dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-# dice_num = randint(1, 6)
+# dice_num = randint(0, 6)
 # print(dice_imgs[dice_num])
 
 # # Play Computer
@@ -19,17 +19,19 @@
 #   print("You are a millenial.")
 # elif year > 1994:
 #   print("You are a Gen Z.")
+# else:
+#   print("Ok Boomer.")
 
 # # Fix the Errors
-# age = input("How old are you?")
-# if age > 18:
-# print("You can drive at age {age}.")
+# age = int(input("How old are you?"))
+# print(f"You can drive at age {age}.") if age >= 18 else print("You are too young.")
+    
 
 # #Print is Your Friend
 # pages = 0
 # word_per_page = 0
 # pages = int(input("Number of pages: "))
-# word_per_page == int(input("Number of words per page: "))
+# word_per_page = int(input("Number of words per page: "))
 # total_words = pages * word_per_page
 # print(total_words)
 
@@ -38,7 +40,7 @@
 #   b_list = []
 #   for item in a_list:
 #     new_item = item * 2
-#   b_list.append(new_item)
+#     b_list.append(new_item)
 #   print(b_list)
 
 # mutate([1,2,3,5,8,13])
